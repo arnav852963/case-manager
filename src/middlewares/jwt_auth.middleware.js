b885f1d04bb98e6e7bd6aace8f1dca12f3b424ea
ArnavTicku <arnavticku01@gmail.com>
2025-10-24 02:51:20 +0530

    initialised fiorebase added jwt auth and role based auth middlewares

---
+++ src/middlewares/jwt_auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import {asyncHandler} from "../utilities/asyncHandler.js";
+import {ApiError} from "../utilities/ApiError.js";
+const jwt_auth = asyncHandler(async (req,_,next)=>{
+    try {
+        const token = req?.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if (!token) throw new ApiError("please login to access this resource");
+
+        const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        if (!decodedToken) throw new ApiError("Invalid token");
+        const user = await prisma.findUnique({
+            where:{
+                id:decodedToken?._id
+            }
+        })
+        if(!user) throw new ApiError(500 , "user not found ")
+        req.user = user;
+        next();
+
+
+    } catch (e) {
+        throw new ApiError(401, `error in jwt_auth middleware -->${e.message}`);
+
+    }
+})
